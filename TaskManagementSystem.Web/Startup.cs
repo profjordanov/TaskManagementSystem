8c3eae714c463d82998b22d64962fdf34cdd4713
Jordan Jordanov <d_yugioh@abv.bg>
2017-05-22 18:34:52 +0300

    Add project files.

---
+++ TaskManagementSystem.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TaskManagementSystem.Web.Startup))]
+namespace TaskManagementSystem.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
